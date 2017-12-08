9e5d02a4d1e5336b5931140c3c3bc7b7ddf105ee
wongwill86-mv-2 <wongwill86@gmail.com>
2017-12-08 00:23:35 -0500

    only install nvidia drivers if they don't exist

--- latest/swarm/install-nvidia-docker.sh
+++ latest/swarm/install-nvidia-docker.sh
@@ -4,8 +4,17 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
 curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docker.list | \
   tee /etc/apt/sources.list.d/nvidia-docker.list
 
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+
 # Add the package repositories
 apt-get update
 
-# Install nvidia-docker2 and reload the Docker daemon configuration
-DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install nvidia-docker2
+if ! nvidia-smi; then
+  apt install nvidia-387
+else
+  echo 'nvidia-smi already installed, not installing drivers'
+fi
+
+# Install nvidia-docker2 
+sudo apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install nvidia-387 nvidia-docker2

776bbb8ac2aa8b543dd61b6dfbb40feaf08eafc6
Ran Lu <ranl@princeton.edu>
2018-02-21 00:30:50 -0500

    Revert "Debug"
    This reverts commit be209beaf059651c2cf90fa137b0122f2ecbcb4e.

--- latest/swarm/install-docker.sh
+++ latest/swarm/install-docker.sh
@@ -15,8 +15,7 @@ apt-get install -y jq
 # nvidia-docker only supports stable docker releases, can't use script
 #wget -qO- https://get.docker.com/ | sh
 
-#apt install docker-ce -y
-apt install -y --allow-downgrades docker-ce=17.06.2~ce-0~ubuntu
+apt install docker-ce -y
 
 sudo usermod -aG docker {{ var "/local/docker/user" }}
 
